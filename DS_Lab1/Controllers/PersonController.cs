bafde0a0ab7632b950f80f9bd59cb187fe3faf15
Ivan Malyshev <thefuny@mail.ru>
2023-10-18 01:33:32 +0300

    init

---
+++ DS_Lab1/Controllers/PersonController.cs
@@ -0,0 +1,41 @@
+﻿using DS_Lab1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DS_Lab1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : Controller
+    {
+        [HttpGet]
+        public Person GetPerson(int personId)
+        {
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Person> GetPersons()
+        {
+
+        }
+
+        [HttpPost]
+        public void CreatePerson(Person person)
+        {
+
+        }
+
+        [HttpPatch]
+        public void UpdatePerson(Person person)
+        {
+
+        }
+
+        [HttpDelete]
+        public void DeletePerson(int personId) 
+        {
+
+        }
+    }
+}
---
+++ DS_Lab1/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace DS_Lab1.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Adress { get; set; }
+        public string Work { get; set; }
+    }
+}
